b8a64206caebc60cff2f3a193587843e6fa27c64
Mauricio Rodrigues <143841940+mauriciorodrigues-dev@users.noreply.github.com>
2023-09-12 19:50:03 -0300

    conclusão da primeira parte

---
+++ EXERCICIOS - TODOS/EXERCICIOS_TIPOS_DE_DADOS/Ex_Tipo_Dado.js
@@ -0,0 +1,22 @@
+// Exercício 1
+console.log("Mauricio")
+console.log('Mauricio')
+console.log(`A soma de 5 + 3 é: ${5 + 3}`)
+
+// Exercício 2
+console.log(5 + 5) //Res: 10
+console.log(8 * 17) //Res: 136
+console.log('Seu IMC é: ' + 89 / (1.7 * 1.7)) //Res: 30.8 - IMC
+
+// Exercício 3
+console.log(5 > 3) //true
+console.log(10 <= 10) //true
+console.log('Mauricio' != 'Maurício') //true
+
+// Exercício 4
+console.log('Mauricio' == 'Mauricio' && 'Mauricio' == 'Mauricio') //true
+console.log(12 + 1 == 15 || 10 < 8) //false
+console.log(!(2 === 2)) //false
+
+//Exercício 5
+console.log(415  * 'NaN')
