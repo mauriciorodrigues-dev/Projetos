c101a743d8a6254c410cb8da518c1d704edf2cbf
Mauricio Rodrigues <143841940+mauriciorodrigues-dev@users.noreply.github.com>
2023-09-12 20:09:10 -0300

    alteração de local

---
+++ Parte Dois/1_Variaveis/script.js
@@ -0,0 +1,18 @@
+let nome = 'Mauricio'
+console.log(nome)
+console.log(`Meu nome é ${nome}`)
+
+let laranja = 12.5
+console.log(laranja * 2)
+
+nome = 'Lucas'
+console.log(nome)
+
+laranja = 12221
+console.log(laranja * 8)
+
+laranja = 'laranja'
+console.log(laranja)
+
+let um = 22, dois = 19, tres = 5
+console.log(um + dois + tres)
