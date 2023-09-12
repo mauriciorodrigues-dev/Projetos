17368f85e91e4a4b5104180ad9de06e108d1c589
Mauricio Rodrigues <143841940+mauriciorodrigues-dev@users.noreply.github.com>
2023-09-12 18:28:42 -0300

    booleans

---
+++ 6_Booleans/script.js
@@ -0,0 +1,49 @@
+console.log(3 > 2)
+console.log(3 == 2)
+console.log(3 < 2)
+console.log('Mauricio' == 'Mauricio')
+console.log('Mauricio' == 'Rodrigues')
+// alert('Mauricio' == 'Mauricio')
+// alert('Mauricio' == 'Rodrigues')
+
+
+console.log(typeof (3 > 2))
+console.log(typeof (3 == 2))
+console.log(typeof (3 < 2))
+
+// Booleans: Comparação
+console.log(2 == 2)
+console.log(3 >= 4)
+console.log('Mauricio' != 'Rodrigues')
+
+// Booleans Operadores Lógicos - Exemplo
+console.log(5 > 3 && 3 == 2)
+console.log(5 > 3 || 3 == 1)
+
+console.log(3 == 3 && 'Mauricio' == 'Mauricio')
+console.log('Joao' == 'Rodrigo' || false)
+console.log(true && true)
+console.log(!(!(true && true)))
+
+console.log(true && true)
+console.log(false || false)
+
+// A e B
+
+let a = true
+let b = false
+
+console.log(a && b)
+console.log(!a)
+
+
+// Booleans: operador ternário
+console.log(2 >= 2 ? 'É sim' : 'É não')
+console.log(5 >= 2 ? 'É sim' : 'É não')
+console.log(5 >= 40 ? 'É sim' : 'É não')
+console.log(20 + 30 == 40 ? 'Sim' : 'Não')
+console.log(20 * 30 == 600 ? 'Sim' : 'Não')
+console.log(20 / 30 == 1 ? 'Sim' : 'Não')
+console.log(false ? 5 : 4)
+console.log('Mauricio' == 'Mauricio'?'Seja bem-vindo, Maurício':'Usuário não encontrado/Cadastrado')
+// alert('Mauricio' == 'Mauricio'?'Seja bem-vindo, Maurício':'Usuário não encontrado/Cadastrado')
