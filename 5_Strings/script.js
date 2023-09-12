e2a4e07ea14c228b22eaddd858b537ddc2086fff
Mauricio Rodrigues <143841940+mauriciorodrigues-dev@users.noreply.github.com>
2023-09-12 11:59:42 -0300

    atualização

---
+++ 5_Strings/script.js
@@ -0,0 +1,16 @@
+console.log('Maurcio')
+console.log(`Mauricio`)
+console.log('Não quero a Maldição do Hello World na minha vida de programador')
+console.log('O resultado de 120 + 233 é: ' + (120+233))
+
+
+console.log(typeof 'Maurcio')
+console.log(typeof `Mauricio`)
+console.log(typeof 'Não quero a Maldição do Hello World na minha vida de programador')
+console.log(typeof 'O resultado de 120 + 233 é: '+' ' + typeof (120+233))
+
+// String Avançado
+console.log('Esta frase vai \npular uma linha rsrsrs')
+console.log(`A soma de 2 + 2 é ${2+2}`)
+// alert('Esta frase vai \npular uma linha rsrsrs')
+console.log('Mauricio ' + 'Rodrigues')
