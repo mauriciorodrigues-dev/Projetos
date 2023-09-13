b22407d88f83245a8b7a03ff9f83c145a9f83186
Mauricio Rodrigues <143841940+mauriciorodrigues-dev@users.noreply.github.com>
2023-09-13 11:44:52 -0300

    atualização

---
+++ FUNCOES/Prompt/script.js
@@ -0,0 +1,22 @@
+// let idade = prompt('Digite sua Idade:')
+// console.log(idade)
+
+// let nome = prompt('Digite seu nome:')
+// console.log(`O seu nome é ${nome} e sua idade é ${idade}`)
+
+// Função Alert
+// alert(`O seu nome é ${nome} e sua idade é ${idade}`)
+
+// Função Math
+let maiorNumero = Math.max(1,2,3,6)
+console.log(maiorNumero)
+
+let menorNumero = Math.min(2, 1, 4, -9)
+console.log(menorNumero)
+
+let arredondar = Math.round(1.3242)
+console.log(arredondar)
+
+let arredondarParaCima = Math.ceil(4.333903)
+console.log(arredondarParaCima)
+
---
+++ Parte Dois/2_Variaveis_Const_Var/script.js
@@ -0,0 +1,24 @@
+var nome = 'Mauricio'
+const ip = '127.5.5.21'
+console.log('O seu nome é ' + nome + ' e seu Ip é ' + ip)
+
+// Convenção de Nomes de Variáveis
+// let 3nome='teste' //apontando erro
+let nome3 = 'teste - 1'
+console.log(nome3)
+let $nome = 'teste - 2'
+console.log($nome)
+let _nome = 'teste - 3'
+console.log(_nome)
+let Nome = 'teste - 4'
+console.log(Nome)
+let NomeTesteHoje = 'Teste - 5'
+console.log(NomeTesteHoje)
+
+// Nomes de Variáveis reservados
+// let if = 'Teste If' - erro
+// let with = 'Teste with - erro
+let case1 = 'Teste Correto'
+console.log(case1)
+let classNumeral = 1222
+console.log(classNumeral)
